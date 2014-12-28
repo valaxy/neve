db8e60b18d1b5b7efdb160b30c871b7d4c333d82
valaxy <valaxy.tian@gmail.com>
2014-12-28 18:29:30 +0800

    some code not finish

--- src/home/index.js
+++ src/home/index.js
@@ -1,9 +1,51 @@
 define(function (require, exports) {
+	var childProcess = requireNode('child_process')
+	var fs = requireNode('fs')
+
+	var Timer = require('../../bower_components/timer/src/timer')
+
 	exports.init = function () {
 		//var Model = require('../treeui/tree-model')
 
 		//require('../process/process')
-		var fs = require('fs')
+		var fs = requireNode('fs')
+
+
+		var INPUT_FILE = 'f://test/test.md'
+		var OUTPUT_FILE = 'f://test/test.html'
+
+
+		var editor = ace.edit($('.editor')[0])
+		editor.setTheme("ace/theme/monokai");
+		editor.getSession().setMode("ace/mode/javascript");
+
+		//var timer = new Timer({
+		//	interval: 1000 * 5,
+		//	immediate: true,
+		//	task: function () {
+		//		fs.writeFile(INPUT_FILE, editor.getValue(), function (err) {
+		//			if (err) {
+		//				$('.preview').html(err)
+		//			}
+		//
+		//			childProcess.exec('pandoc -f markdown -t html ' + INPUT_FILE, function (error, stdout, stderr) {
+		//				$('.preview').html(stdout)
+		//				console.log(timer)
+		//				timer.next()
+		//			})
+		//		})
+		//	}
+		//})
+		//
+		//timer.start()
+
+		var watch = requireNode('../../node_modules/watch/main')
+		watch.watchTree('f://test', function (f, curr, prev) {
+			console.log(f, curr, prev)
+		})
+
+
+
 
 
 	}
