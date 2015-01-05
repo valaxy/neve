0e5e8025cfef4542a726b25fb2f51dc5445ddc76
valaxy <valaxy.tian@gmail.com>
2015-01-05 21:38:35 +0800

    refactor layout

--- src/home/index.js
+++ src/home/index.js
@@ -1,45 +1,17 @@
 define(function (require, exports) {
 	var path = requireNode('path')
 
-	var boxLayout = require('../../bower_components/jquery-box-layout/src/box-layout')
 	var process = require('../process/process')
 	var TreeView = require('../file-tree/file-tree-view')
 	var FileTreeModel = require('../file-tree/file-tree-model')
 	var TopNavView = require('../top-nav/index')
 	var g = require('./global')
 	var editor = require('../editor/editor')
+	var layout = require('./layout')
 
 
 	exports.init = function () {
-
-		var $dom = boxLayout.init({
-			_schema: 'linear',
-			isHor: false,
-			boxes: [{
-				_schema: 'box',
-				size: 20,
-				domSelector: '.top-nav'
-			}, {
-				_schema: 'linear',
-				size: 'auto',
-				isHor: true,
-				boxes: [{
-					_schema: 'box',
-					size: 200,
-					domSelector: '.tree'
-				}, {
-					_schema: 'box',
-					size: 600,
-					domSelector: '.editor'
-				}, {
-					_schema: 'box',
-					size: 'auto',
-					domSelector: '.preview'
-				}]
-			}]
-		})
-		$('.everything').append($dom)
-
+		layout.init()
 		editor.init()
 
 		// the file tree
--- src/home/layout.js
+++ src/home/layout.js
@@ -1,3 +1,43 @@
 define(function (require, exports) {
 
+	var boxLayout = require('bower_components/jquery-box-layout/src/box-layout')
+
+	exports.init = function () {
+		var $dom = boxLayout.init({
+			_schema: 'linear',
+			isHor: false,
+			size: 'auto',
+			isPer: false,
+			boxes: [{
+				_schema: 'box',
+				size: 20,
+				domSelector: '.top-nav'
+			}, {
+				_schema: 'linear',
+				size: 'auto',
+				isHor: true,
+				isPer: false,
+				boxes: [{
+					_schema: 'box',
+					size: 200,
+					domSelector: '.tree'
+				}, {
+					_schema: 'linear',
+					size: 'auto',
+					isHor: true,
+					isPer: true,
+					boxes: [{
+						_schema: 'box',
+						size: 50,
+						domSelector: '.editor'
+					}, {
+						_schema: 'box',
+						size: 'auto',
+						domSelector: '.preview'
+					}]
+				}]
+			}]
+		})
+		$('.everything').append($dom)
+	}
 })
