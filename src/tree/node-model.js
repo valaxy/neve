9fc6ddff39262df6d66942843d3573c159a40598
valaxy <valaxy.tian@gmail.com>
2015-01-17 14:51:08 +0800

    add function node-model.js

--- src/tree/node-model.js
+++ src/tree/node-model.js
@@ -1,11 +1,13 @@
 define(function () {
 
-	/**
-	 * NodeModel used in TreeModel
-	 * attribute value is a the in fact value of NodeModel
-	 */
 	var NodeModel = Backbone.RelationalModel.extend({
+		/**
+		 * children is the children of node.
+		 */
 
+		/**
+		 * parent is the parent of node.
+		 */
 		relations: [{
 			key: 'children',
 			type: Backbone.HasMany,
@@ -16,8 +18,51 @@ define(function () {
 
 		isLeaf: function () {
 			return this.get('children').length == 0
+		},
+
+		/** add child and return this */
+		addChild: function (child) {
+			this.get('children').add(child)
+			return this
+		},
+
+		/** remove and return this */
+		remove: function () {
+			if (this.get('parent')) {
+				return this.get('parent').get('children').remove(this)
+			} else {
+				return this
+			}
 		}
 	})
+
+
+	if (typeof QUnit != 'undefined') {
+		QUnit.module('NodeModel')
+
+		QUnit.test('addChild()/remove()/parent/children', function (assert) {
+			var root = new NodeModel
+			assert.equal(root.get('children').length, 0)
+
+			var n1 = new NodeModel
+			var n2 = new NodeModel
+			var n3 = new NodeModel
+			root.addChild(n1)
+			root.addChild(n2)
+			n2.addChild(n3)
+
+			assert.equal(root.get('parent'), null)
+			assert.equal(n1.get('parent'), root)
+			assert.equal(n2.get('parent'), root)
+			assert.equal(n3.get('parent'), n2)
+			assert.equal(root.get('children').length, 2)
+
+			n2.remove()
+			assert.equal(root.get('children').length, 1)
+			assert.equal(n2.get('parent'), null)
+		})
+
+	}
 
 	return NodeModel
 })
--- test/mod/test.js
+++ test/mod/test.js
@@ -1,10 +1,12 @@
 define(function (require, exports) {
 	exports.init = function () {
-		require('../../src/utility/utility')
+		require('src/utility/utility')
 
-		require('../../src/tree/node-model')
-		require('../../src/tree/tree-model')
+		require('src/tree/node-model')
+		require('src/tree/tree-model')
 
-		require('../../src/file-tree/file-tree-view')
+		//require('src/file-tree/file-tree-view') // test error can't locate
+		require('../file-tree/file-model')
+
 	}
 })
