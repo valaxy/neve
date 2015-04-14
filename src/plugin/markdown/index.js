8da1e97c23efe1237af2544cb6d58e78fce367ed
tianyuzou <valaxy@qq.com>
2015-04-14 20:58:18 +0800

    refactor little

--- src/plugin/markdown/index.js
+++ src/plugin/markdown/index.js
@@ -8,28 +8,29 @@ var $__index_46_es6_46_js__ = (function() {
     var markdown = require('bundle!marked');
     var loader = require('../../loader/index');
     var layout = require('../../home/layout');
-    var Editor = require('../../editor/editor');
+    var EditorView = require('../../editor/editor');
     var fileWatcherLoader = require('../../loader/file-watcher-loader');
+    var layoutLoader = require('../../loader/layout-loader');
     exports.init = function(options) {
       this._projectManager = options.projectManager;
-      this._projectManager.on('open', function() {
-        loader.load('html!../editor/index', 'html!../preview/index', 'html!../editor-tab/index').done(function(editor, preview, editorTab) {
-          var linear = new LinearLayout({direction: 'column'});
-          var linear2 = new LinearLayout({direction: 'row'});
-          var editor = new SimpleView({selector: $(editor)});
-          var preview = new SimpleView({selector: $(preview)});
-          var editorTab = new SimpleView({selector: $(editorTab)});
-          linear2.appendView(editor, {flex: 1});
-          linear2.appendView(preview, {flex: 1});
-          linear.appendView(editorTab, {flex: '30px'});
-          linear.appendView(linear2, {flex: 1});
-          layout.appendAfterFileTree(linear, {flex: 1});
-          var editorView = new Editor({
-            el: editor._$dom,
-            projectManager: options.projectManager
-          });
+      layoutLoader.load();
+      loader.load('html!../editor/index', 'html!../preview/index', 'html!../editor-tab/index').done(function(editorHTML, previewHTML, editorTabHTML) {
+        var linear = new LinearLayout({direction: 'column'});
+        var linear2 = new LinearLayout({direction: 'row'});
+        var editor = new SimpleView({selector: $(editorHTML)});
+        var preview = new SimpleView({selector: $(previewHTML)});
+        var editorTab = new SimpleView({selector: $(editorTabHTML)});
+        linear2.appendView(editor, {flex: 1});
+        linear2.appendView(preview, {flex: 1});
+        linear.appendView(editorTab, {flex: '30px'});
+        linear.appendView(linear2, {flex: 1});
+        layout.appendAfterFileTree(linear, {flex: 1});
+        var editorView = new EditorView({
+          el: editor._$dom,
+          projectManager: options.projectManager
         });
       });
+      this._projectManager.on('open', function() {});
       this._projectManager.on('close', function() {});
       fileWatcherLoader.load({
         name: 'markdown',
